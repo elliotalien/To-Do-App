7230129163d1c0b773b9d282800c3655454a6013
elliotalien <blackdevilhacker4@gmail.com>
2024-10-10 00:40:24 +0530

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Todo } from './components/Todo'
 
 const App = () => {
   return (
-    <div className='bg-stone-900 grid p-4 min-h-screen'>
+    <div className='bg-stone-900 flex justify-center lg:p-4 min-h-screen'>
       <Todo/>
     </div>
   )
